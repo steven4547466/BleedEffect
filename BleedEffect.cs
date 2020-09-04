7ee0924f53d05bc72ffc9f77f70702b9213c9a2c
Steven Webster <steven@is-just-a.dev>
2020-09-04 14:06:23 -0400

    Update version info

--- BleedEffect.cs
+++ BleedEffect.cs
@@ -16,7 +16,7 @@ namespace BleedEffect
         public override PluginPriority Priority { get; } = PluginPriority.Medium;
         public override string Name { get; } = "BleedEffect";
         public override string Author { get; } = "Steven4547466";
-        public override Version Version { get; } = new Version(1, 0, 0);
+        public override Version Version { get; } = new Version(1, 0, 1);
         public override Version RequiredExiledVersion { get; } = new Version(2, 1, 2);
         public override string Prefix { get; } = "BleedEffect";
 
